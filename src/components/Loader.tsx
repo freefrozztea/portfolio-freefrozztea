75fa86c8c06d503fde34f5fe1e7ed4daf9b25000
lalzamor <lalzamor@everis.com>
2022-09-02 01:26:08 -0300

    add some animations

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,7 +4,7 @@ import './Loader.css';
 const Loader = () => {
   return (
     <div className='loader-container'>
-      <span>Cargando </span>
+      <span className='loader-text'>Cargando </span>
       <span className="loader"></span>
     </div>
   );
